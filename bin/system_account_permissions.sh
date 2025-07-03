6b9c10292f7aeff8ebbd2c96c4969e0dd29c7f23
Eric Passmore <eric.passmore@gmail.com>
2025-07-03 09:26:09 -0700

    Corrected system permissions for all *.eosio account

---
+++ bin/system_account_permissions.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+ENDPOINT_ONE=$1
+WALLET_DIR=$2
+ROOT_PUBLIC_KEY=${3}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+"${SCRIPT_DIR}"/open_wallet.sh "$WALLET_DIR" root
+
+cat > $HOME/eosio_accounts_owner_auth.json << EOF
+{
+  "threshold": 1,
+  "keys": [
+    {
+      "key": "${ROOT_PUBLIC_KEY}",
+      "weight": 1
+    }
+  ],
+  "accounts": [
+    {
+      "permission": {
+          "actor": "eosio",
+          "permission": "active"
+      },
+      "weight": 1
+    }
+  ],
+  "waits": []
+}
+EOF
+cat > $HOME/wram_active_auth.json << EOF
+{
+  "threshold": 2,
+  "keys": [],
+  "accounts": [
+    {
+      "permission": {
+          "actor":"eosio",
+          "permission": "active"
+      },
+      "weight": 1
+    },
+    {
+      "permission": {
+          "actor": "eosio.prods",
+          "permission": "prod.minor"
+      },
+      "weight": 1
+    },
+  ],
+  "waits": []
+}
+EOF
+cat > $HOME/eosio_accounts_active_auth.json << EOF
+{
+  "threshold": 1,
+  "keys": [],
+  "accounts": [
+    {
+      "permission": {
+          "actor":"eosio",
+          "permission": "active"
+      },
+      "weight": 1
+    }
+  ],
+  "waits": []
+}
+EOF
+
+# set active to eosio@active 
+for account in eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.vpay eosio.rex eosio.reserv; do
+   cleos --url $ENDPOINT_ONE set account permission ${account} active $HOME/eosio_accounts_active_auth.json -p${account}@owner
+done
+
+# wram special
+cleos --url $ENDPOINT_ONE set account permission eosio.wram active $HOME/wram_active_auth.json -peosio.wram
+
+# enable set code 
+for account in eosio.bpay eosio.saving eosio.fees eosio.reward eosio.wram; do
+  cleos -u $ENDPOINT_ONE set account permission ${account} active --add-code -p${account}@owner
+  cleos -u $ENDPOINT_ONE push action eosio setpriv "[\"${account}\", 1]" -peosio
+done
+
+# set owner to eosio@active
+for account in eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.vpay eosio.rex eosio.fees eosio.reward eosio.wram eosio.reserv; do
+    cleos --url $ENDPOINT_ONE set account permission ${account} owner $HOME/eosio_accounts_active_auth.json -p${account}@owner
+done
+
+for account in eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.vpay eosio.rex eosio.fees eosio.reward eosio.wram eosio.reserv; do
+  echo "${account}---------------"
+  cleos -u https://jungle4.cryptolions.io:443 get account $account -j | jq .permissions[1].required_auth.accounts
+done
+
